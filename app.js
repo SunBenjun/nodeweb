7c15e05bbbd9a8f6815141b3ec5d5bc8a27ebb3c
sunbenjun <sbj430@qq.com>
2017-04-03 00:51:23 +0800

    “添加修改电影本地通过

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-var express  = require('express'); //加载express模块
-var bodyParser = require('body-parser'); //用来格式表单数据 4.几版本后与express分离所以要单独引用 
-var mongoose = require('mongoose'); //引入mongoose数据库模块 来连接数据库
-var Movie = require('./models/movie'); //把模型加载进来
-var _ = reuqire('underscore');
-var port = process.env.PORT || 3000;  //设置端口 process.env.PORT  process是全局变量    可以使用 PORT=4000 node app.js 启动端口号
-var path = require('path'); //静态资源寻找路径
-var app = express();   //启动一个web服务器 实例附给app
-mongoose.connect('mongodb://localhost/imooc'); //mongoose connect调用数据库 数据库名为imooc
-app.set('views','./views/pages');  //设置视图默认目录
-app.set('view engine','jade');  //设置 默认模板引擎 jdade
-//app.use(express.bodyParser()); //后台有提交表单 这个是格式化表单
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname,'bower_components')));//静态资源从这里找
-app.locals.moment = require('moment'); //list.jade 里面调用了moment格式化时间
-app.listen(port);  //监听端口
-console.log('imooc started on port'+port); //打印一条日志判断服务是否启动了
+var express = require('express');//加载express模块
+var path = require('path');//静态资源寻找路径
+var _ = require('underscore');
+var mongoose = require('mongoose');//引入mongoose数据库模块 来连接数据库
+var Movie = require('./models/movie');//把模型加载进来
+var bodyParser = require('body-parser');//用来格式表单数据 4.几版本后与express分离所以要单独引用 
+var port = process.env.PORT || 3000;
+var app = express();//启动一个web服务器 实例附给app
+// mongoose.Promise = require('bluebird');
+mongoose.connect('mongodb://localhost:27017/imooc');//mongoose connect调用数据库 数据库名为imooc
 
+app.set('views','./views/pages');//设置视图默认目录
+app.set('view engine','jade');//设置 默认模板引擎 jdade
+app.use(bodyParser.json() );
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname,'public')));//静态资源从这里找
+app.locals.moment = require('moment');//list.jade 里面调用了moment格式化时间
+app.listen(port);//监听端口
+console.log('Setver is running at ' + port);//打印一条日志判断服务是否启动了
 //index page 加入页面 路由
 app.get('/',function(req,res){
 	// var movies = [{
@@ -43,17 +44,16 @@ app.get('/',function(req,res){
 	// 		_id:6,
 	// 		poster:'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
 	// 	}]
-	Movie.fetch(function(err,movies){
-		if(err){
-			console.log(err)
-		}
-		res.render('index',{
-			title:'imooc 首页',
-			movies:movies
-		});
-	});
-});
-
+  Movie.fetch(function(err,movies) {
+    if(err){
+      console.log(err);
+    }
+    res.render('index',{
+      title:'MOMO 首页',
+      movies:movies
+    })
+  })
+})
 //detail page 加入页面 路由
 app.get('/movie/:id',function(req,res){
 	// var movie = {
@@ -66,84 +66,103 @@ app.get('/movie/:id',function(req,res){
 	// 	flash:'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf',
 	// 	summary:'翻拍自1987年同名科幻经典，由《精英部队》导演'	
 	// }
-	var id = req.params.id;
-	Movie.findById(id,function(err,movie){
-		res.render('detail',{
-			title:'imooc'+movie.title,
-			movie: movie
-		});
-	});
+  console.log('enter detail');
+  var id = req.params.id;
+  Movie.findById(id,function(err,movie) {
+    if(err) {
+      console.log(err);
+    }
+    res.render('detail',{
+      title:'MOMO'+movie.title,
+      movie:movie
+    })
+  })
+})
 
-});
+app.get('/admin/update/:id',function(req,res) {
+  var id = req.params.id;
+  if(id){
+    Movie.findById(id,function(err,movie) {
+      res.render('admin',{
+        title:"immoc 后台更新页",
+        movie:movie
+      })
+    })
+  }
+})  
 
-//admin page 加入页面 路由
 app.get('/admin/movie',function(req,res){
-	res.render('admin',{
-		title:'imooc 后台录入页',
-		movie:{
-			title:'',
-			doctor:'',
-			country:'',
-			year:'',
-			poster:'',
-			flash:'',
-			summary:'',
-			language:''
-		}
-	});
-});
-//admin update movie
-app.get('/admin/update/:id',function(req,res){
-	var id = req.params.id;
-	if(id){
-		Movie.findById(id,function(err,movie){
-			res.render('admin',{
-				title:'imooc 后台更新页'，
-				movie:movie
-			});
-		});
-	}
-});
-// admin post movie 从表单post过来的
-app.post('/admin/movie/new',function(res,req){
-	var id = req.body.movie._id; //用来判断是新增的还是修改
-	var moveObj = req.body.movie;
-	var _movie;
-	if(id!=="undefined"){ //有id是修改
-		Movie.findById(id,function(err,movie){
-			if(err){
-				console.log(err);
-			}
-			//以下引用了underscore的内容 所以需要引用 underscore
-			_movie = _.extend(movie,movieObj);
-			_movie.save(fnction(err,movie){
-				if(err){
-					console.log(err);
-				}
-				res.redirect('/movie/'+movie._id); //更新成功跳转到电影页面
-			});
-		})
-	}else{//没有id是新增
-		_movie = new Movie({
-			doctor:movieObj.doctor,
-			title:movieObj.title,
-			country:movieObj.country,
-			language:movieObj.language,
-			year:movieObj.year,
-			poster:movieObj.poster,
-			summary:movieObj.summary,
-			flash:movieObj.flash
-		});
-		_movie.save(function(err,movie){
-			if(err){
-				console.log(err)
-			}
-			res.redirect('/movie/'+movie._id); //更新成功跳转到电影页面
-		})
-	}
-});
+  console.log('enter admin');
+  res.render('admin',{
+      title:'MOMO 后台',
+      movie:{
+        doctor:'',
+        country:'',
+        title:'',
+        year:'',
+        poster:'',
+        language:'',
+        flash:'',
+        summary:'',
+      }
+  })
+})
 
-//list page 加入页面 路由
+//admin update
+app.get('/admin/update/:id',function(req,res) {
+  var id = req.params.id;
+  if(id){
+    Movie.findById(id,function(err,movie) {
+      res.render('admin',{
+        title:"immoc 后台更新页",
+        movie:movie
+      })
+    })
+  }
+})
+
+//admin post movie
+app.post('/admin/movie/new',function(req,res) {
+  console.log('enter post');
+  var id = req.body.movie._id;//用来判断是新增的还是修改
+  var movieObj =req.body.movie;
+  var _movie;
+  if(id !== 'undefined'){
+    Movie.findById(id,function(err,movie){
+      if(err){
+        console.log(err);
+      }
+      //以下引用了underscore的内容 所以需要引用 underscore
+      //extend方法 用另一个对象里面的新的字段替换老的对象里面的对应的字段 
+      _movie = _.extend(movie,movieObj)
+      _movie.save(function(err,movie) {
+        if(err){
+          console.log(err);
+        }
+        res.redirect('/movie/'+movie._id);
+      })
+    })
+  }
+  else{
+    _movie = new Movie({
+      doctor:movieObj.doctor,
+      title:movieObj.title,
+      country:movieObj.country,
+      language:movieObj.language,
+      year:movieObj.year,
+      poster:movieObj.poster,
+      summary:movieObj.summary,
+      flash:movieObj.flash
+    })
+    _movie.save(function(err,movie) {
+      if(err){
+        console.log(err);
+      }
+      res.redirect('/movie/'+movie._id);
+    })
+  }
+})
+
 app.get('/admin/list',function(req,res){
 	// var movies = [{
 	// 		title: '机械战警',
@@ -153,15 +172,29 @@ app.get('/admin/list',function(req,res){
 	// 		year: 2014,
 	// 		language: '英语',
 	// 		flash: 'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf'
-	// 	}];
+	// 	}];	
+  Movie.fetch(function(err,movies) {
+    if(err){
+      console.log(err);
+    }
+    res.render('list',{
+      title:'MOMO 列表页',
+      movies:movies
+    })
+  })
+})
 
-	Movie.fetch(function(err,movies){
-		if(err){
-			console.log(err)
-		}
-		res.render('list',{
-			title:'imooc 列表页',
-			movies:movies
-		});		
-	});
-});
+//list
+app.delete('/admin/list',function(req,res) {
+  var id = req.query.id;
+  if(id) {
+    Movie.remove({_id: id},function(err,movie) {
+      if(err){
+        console.log(err);
+      }
+      else{
+        res.json({success: 1});
+      }
+    })
+  }
+})
--- schemas/movie.js
+++ schemas/movie.js
@@ -16,31 +16,30 @@ var MovieSchema = new mongoose.Schema({
 		updateAt:{
 			type:Date,
 			default:Date.now()
-		}		
+		},
 	}
 });
-
-MovieSchema.pre('save',function(next){   // pre('save 的意思是 每次存储数据之前都调用这个方法
-	if(this.isNew){ //判断如果是新加的把创建时间和更新时间设置为当前时间
+MovieSchema.pre('save',function(next) {// pre('save 的意思是 每次存储数据之前都调用这个方法
+	if(this.isNew) {//判断如果是新加的把创建时间和更新时间设置为当前时间
 		this.meta.createAt = this.meta.updateAt = Date.now();
-	}else{
+	}
+	else{
 		this.meta.updateAt = Date.now();
 	}
-	next();  //需要调用next 才会将存储流程走下去
+	next();//需要调用next 才会将存储流程走下去
 });
 //只有model实例化之后 才会具有这个方法 
 MovieSchema.statics = {
-	fetch:function(cb){
+	fetch:function(cb) {
 		return this
 			.find({})
-			.sort('meta.updateAt')   //按照更新时间排序 
-			.exec(cb)  //执行回调方法 
+			.sort('meta.updateAt')//按照更新时间排序 
+			.exec(cb)//执行回调方法 
 	},
-	findById:function(id,cb){ //查询单条数据
+	findById:function(id,cb) {//查询单条数据
 		return this
 			.findOne({_id:id})
 			.exec(cb)
 	}
 }
-
-modue.exports = MovieSchema  //模式导出  模式创建完后要创建模型 model
+module.exports = MovieSchema;//模式导出  模式创建完后要创建模型 model
